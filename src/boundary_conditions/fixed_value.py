0ce5ac6dd4cadd5142a26a25478c1a912ee45f24
cfos3120 <cfos3120@uni.sydney.edu>
2025-05-14 17:53:23 +1000

    fix

--- src/boundary_conditions/fixed_value.py
+++ src/boundary_conditions/fixed_value.py
@@ -1,11 +1,13 @@
 import torch
 import numpy as np
 
-def fixed_value_bc(grad_field: torch.tensor, 
+def fixed_value_bc(grad_field: torch.tensor,
+                   field_values: torch.tensor,
                    owner: torch.tensor,
                    bc_patch: dict,
                    order = 1,
                    dtype = torch.float32,
+                   d_vec:torch.tensor = None,
                    **kwargs) -> torch.tensor:
     '''
     Returns Tensor of shape like ,
@@ -35,6 +37,7 @@ def fixed_value_bc(grad_field: torch.tensor,
         
         # These cell values need to be changed to original U not GRAD(U)
         # The direction here may be wrong of the delta_d (should be pointing out of the cell, but difference is cell - face, might need to swap that)
+        cell_values = field_values[...,owner,:] 
         bc_values_at_face = (cell_values - bc_values_at_face)
         #bc_values_at_face = torch.einsum('itjk,fc->btfci', bc_values_at_face, delta_d_unit_vector).permute(0,1,3,2).flatten(start_dim=2) #<- stuck here
         # we need Grad(U) at face = Delta(U)/Delta(D) keeping dimension and then flatten so that we have 
